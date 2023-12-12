92c5562793e8387ab67949aa43e15dff9bada187
ojomaa <omarjomaa21@gmail.com>
2023-12-11 20:41:14 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Confetti from "react-confetti"
+import Die from "./Die"
+
+export default function App() {
+  return (
+    <>
+      <Die />
+    </>
+  )
+}
---
+++ src/Die.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+export default function Die() {
+    return(
+        <h1> This is the Die Function </h1>
+    )
+}
