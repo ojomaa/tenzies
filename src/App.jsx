140727ec4165b578a243cf0a020432789e40cd24
ojomaa <omarjomaa21@gmail.com>
2023-12-14 14:46:18 -0500

    holdDice functionality and code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,60 @@
 import React from "react"
 import Confetti from "react-confetti"
 import Die from "./Die"
+import { nanoid } from "nanoid"
 
 export default function App() {
 
-  // States
+  // States //
   const [diceNum, setDiceNum] = React.useState(allNewDice())
 
-  // Effects
+  // Variables //
+  const diceNumbers = diceNum.map((die) => (
+    <Die 
+      key={die.id} 
+      value={die.number}
+      isHeld={die.isHeld}
+      holdDice = {() => holdDice(die.id)}
+    />
+))
 
-  // Functions
+  // Effects //
+
+  // Functions //
+
+  function generateDice() {
+    return {
+        number: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+}
   function allNewDice() {
-    return Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6))
+
+    return Array.from({ length: 10 }, () => {
+      return generateDice()
+    })
   }
 
   function rollDice() {
-    setDiceNum(allNewDice())
-  }
-
-  const diceNumbers = diceNum.map((num, index) => (
-        <Die key={index} value={num} />
+    setDiceNum(oldDice => oldDice.map(dice => {
+        return dice.isHeld ? dice : generateDice()
+        }
     ))
+}
+
+  function holdDice(id) {
+    setDiceNum(oldDice => oldDice.map(die => {
+      return die.id === id ? 
+          {...die, isHeld: !die.isHeld} :
+          die
+  }))
+  }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='dice-container'>
         {diceNumbers}
       </div>
--- src/Die.jsx
+++ src/Die.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 export default function Die(props) {
     return(
-        <div className='dice'>
+        <div className={`dice ${props.isHeld ? 'hold' : '' }`} onClick={props.holdDice}>
             <h2>{props.value}</h2>
         </div>
     )
